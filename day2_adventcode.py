1daf4ba47d0cec5169ceeb060a420c1715764673
dave <doparko@gmail.com>
2019-10-05 18:21:47 -0700

    Working code to solve the problem. Sadly I have a for loop imbedded in another for loop, eeeek

--- day2_adventcode.py
+++ day2_adventcode.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Oct  5 15:48:19 2019
 
@@ -11,12 +10,35 @@ import os
 import numpy as np
  
 filename = "day2_input.txt"
-folder = "C:/Users/dopar/Documents/advent_of_code/"
 
-datain = open(folder + filename,'r')
+datain = open(filename,'r')
 
-text = datain.readlines() 
-print(text[0])
-print(text[1])
+text = datain.readlines()
+
+dubs = 0
+trips = 0
+i = 0
+
+xsort = [None] * len(text)
+
+for x in text:
+    xsort[i] = sorted(x)
+    
+    checkdub = 0
+    checktrip = 0
+    
+    for j in xsort[i]:
+        if xsort[i].count(j) == 2 and checkdub == 0:
+            dubs = dubs + 1
+            checkdub = 1
+        elif xsort[i].count(j) == 3 and checktrip == 0:
+            trips = trips + 1
+            checktrip = 1
+            
+    i = i + 1
+
+checksum = dubs * trips
+
+print(checksum)
 
 datain.close()
