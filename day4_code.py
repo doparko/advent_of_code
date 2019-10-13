ba4c79f46bdd347e7f391c3bd7e8fe9d16b5345e
D Chris <doparko@gmail.com>
2019-10-13 12:25:16 -0700

    Got the basic sorting to work but not able to sort furth beyond month consistently

--- day4_code.py
+++ day4_code.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-filename = 'day4_short_input.txt'
+filename = 'day4_input.txt'
 datas = open(filename)
 data = datas.readlines()
 datas.close()
@@ -15,10 +15,10 @@ def mdorder(datin,ins,ends):
     for ent in datin:
         dm = int(ent[ins:ends])
         if len(ordlist) == 0:
-            ordlist.append(ent)
+            ind = 0
         else:
             ind = biplace(ordlist,dm,ins,ends)
-            ordlist.insert(ind,ent)
+        ordlist.insert(ind,ent)
     return ordlist
 
 def biplace(plist,numb,inns,endds):
@@ -26,16 +26,58 @@ def biplace(plist,numb,inns,endds):
     end = len(plist)
     sett = 0
     while sett == 0:
-        diff = int((end-beg)/2)
-        if numb == int((plist[diff])[inns:endds]):
+        diff = int((end+beg)/2)
+        if beg == end:
+            sett = 1
+            index = beg
+        elif numb == int((plist[diff])[inns:endds]):
             sett = 1
             index = diff
+        elif beg == end:
+            sett = 1
+            index = beg
         elif numb > int((plist[diff])[inns:endds]):
-            beg = diff
+            beg = int(np.ceil((end+beg)/2))
         else:
             end = diff
     return index
 
-ordmonth = mdorder(data,6,8)
-ordday = mdorder(data,9,11)
+ordmonth = mdorder(data,6,8)      # Orders months
+ordday = mdorder(ordmonth,9,11)   # Orders to the day
+ordhour = mdorder(ordday,12,14)   # Orders to the hour
+ordfinal = mdorder(ordhour,15,17) # Final ordering to the final minute
 
+# Write-Overwrites 
+file1 = open("myfile.txt","w")#write mode 
+file1.write(''.join(ordmonth)) 
+file1.close()
+
+
+##
+##ins = 6
+##ends = 8
+##ordlist = []
+
+##for ent in data:
+##    dm = int(ent[ins:ends])
+##    if len(ordlist) == 0:
+##        index = 0
+##    else:
+##        beg = 0
+##        end = len(ordlist)
+##        sett = 0
+##        while sett == 0:
+##            diff = int((end+beg)/2)
+##            if beg == end:
+##                sett = 1
+##                index = beg
+##                break
+##            elif dm == int((ordlist[diff])[ins:ends]):
+##                sett = 1
+##                index = diff
+##                break
+##            elif dm > int((ordlist[diff])[ins:ends]):
+##                beg = int(np.ceil((end+beg)/2))
+##            else:
+##                end = diff
+##    ordlist.insert(index,ent)
