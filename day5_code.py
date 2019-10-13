73e9c7eb4c6821dc6bb2ef1e12d967d2c5b48ad9
D Chris <doparko@gmail.com>
2019-10-13 10:34:50 -0700

    first part of day 5 seems to be working so so, for some reason answer is off by one

---
+++ day5_code.py
@@ -0,0 +1,45 @@
+# Day 5 advent of code
+# will take in some polyer chain
+# types are different letters like 'r' and 's'
+# polarity is represented by its case like 'r' and 'R' being opposite polarity
+# oppoiste polarity annihilates
+
+filename = 'day5_input.txt'
+data = open(filename)
+datain = data.read()
+data.close()
+
+partnum = input("which part number(1/2)?:")
+
+def charmap(let):
+    chcd = ord(let)
+    if chcd < 95:
+        ans = chcd + 32
+    else:
+        ans = chcd - 32
+    return chr(ans)
+
+polist = list(datain)
+done = 0
+popamt = 0
+
+# part 1
+if partnum == 1:
+    while done == 0:
+        for e in range(len(polist)):
+            if e == len(polist) - 1:
+                done = 1
+                break
+            elif polist[e+1] == charmap(polist[e]):
+                polist.pop(e)
+                polist.pop(e)
+                popamt += 2
+                break
+
+    print("The amount of entries left in the polymer is:",len(polist)-1)
+elif partnum == 2:
+    # start part 2 here
+else:
+    print("bad input buddy")
+
+        
