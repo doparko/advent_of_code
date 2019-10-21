76c5b17b0ba0c90df8f1f64b59373b2724020ec6
D Chris <doparko@gmail.com>
2019-10-21 00:40:17 -0700

    day 4 has the ordering working, just need to get the common sleeping minute part done

--- day4_code.py
+++ day4_code.py
@@ -13,7 +13,7 @@ datas.close()
 def mdorder(datin,ins,ends):
     ordlist = []
     for ent in datin:
-        dm = int(ent[ins:ends])
+        dm = int(ent[ins[0]:ends[0]]+ent[ins[1]:ends[1]]+ent[ins[2]:ends[2]]+ent[ins[3]:ends[3]])
         if len(ordlist) == 0:
             ind = 0
         else:
@@ -25,32 +25,57 @@ def biplace(plist,numb,inns,endds):
     beg = 0
     end = len(plist)
     sett = 0
+    count = 0
     while sett == 0:
+        count += 1
         diff = int((end+beg)/2)
+        #print("diff:",diff,"plist length:",len(plist),"begin:",beg,"end:",end)
+        #checknum = int((plist[diff])[inns[0]:endds[0]]+(plist[diff])[inns[1]:endds[1]]+(plist[diff])[inns[2]:endds[2]]+(plist[diff])[inns[3]:endds[3]])
         if beg == end:
             sett = 1
             index = beg
-        elif numb == int((plist[diff])[inns:endds]):
+        elif numb == int((plist[diff])[inns[0]:endds[0]]+(plist[diff])[inns[1]:endds[1]]+(plist[diff])[inns[2]:endds[2]]+(plist[diff])[inns[3]:endds[3]]):
             sett = 1
             index = diff
         elif beg == end:
             sett = 1
             index = beg
-        elif numb > int((plist[diff])[inns:endds]):
+        elif numb > int((plist[diff])[inns[0]:endds[0]]+(plist[diff])[inns[1]:endds[1]]+(plist[diff])[inns[2]:endds[2]]+(plist[diff])[inns[3]:endds[3]]):
             beg = int(np.ceil((end+beg)/2))
         else:
             end = diff
     return index
-
-ordmonth = mdorder(data,6,8)      # Orders months
-ordday = mdorder(ordmonth,9,11)   # Orders to the day
-ordhour = mdorder(ordday,12,14)   # Orders to the hour
-ordfinal = mdorder(ordhour,15,17) # Final ordering to the final minute
+starts = [6,9,12,15]
+ending = [8,11,14,17]
+ordmonth = mdorder(data,starts,ending)      # Orders the data month-day-hour-min
 
 # Write-Overwrites 
 file1 = open("myfile.txt","w")#write mode 
 file1.write(''.join(ordmonth)) 
 file1.close()
+
+minsleep = {}
+
+for ea in ordmonth:
+    ident = ea.split(" ")
+    if ident[2] == "Guard":
+        onguard = ident[3]
+    elif ident[2] == "falls":
+        sleepstart = int(ea[15:17])
+    elif ident[2] == "wakes":
+        sleepend = int(ea[15:17]) - 1
+        if onguard in minsleep:
+            minsleep[onguard] = minsleep[onguard] + (sleepend - sleepstart)
+        else:
+            minsleep[onguard] = sleepend - sleepstart
+
+print("guard that sleeps the most is:", max(minsleep))
+
+for el in ordmonth:
+    maing = (el.split(" "))[2]
+    if maing == "guard":
+        duty = (el.split(" "))[3]
+        
 
 
 ##
