1349f95d389db06cc6026f23158630d04e017a38
dave <doparko@gmail.com>
2021-12-21 11:42:54 -0800

    Figured out day 12, drawing it out really helped!

--- aoc2021/day12_code.py
+++ aoc2021/day12_code.py
@@ -30,18 +30,99 @@ import matplotlib.pyplot as plt
 import re
 import numpy as np
 import math
-filename = 'day12_inputsample.txt'
-#filename = 'day12_input.txt'
+filename = 'day12_inputsample2.txt'
+filename = 'day12_input.txt'
 datin = open(filename)
 datinss = datin.read()
 datin.close()
 
 datas = datinss.split('\n')
 
-def possiblepath(mapp):
-    mystep = mapp['start']
-    for e in mystep:
-                
+def countlower(clist):
+    # Counting occurences of letter in clist
+    if len(clist) < 2:
+        return 0,{clist[0]:1}
+    count = {}
+    for e in clist:
+        if e == e.lower():
+            if e in count:
+                count[e]+=1
+            else:
+                count[e]=1
+    themax = max(list(count.values()))
+    return themax,count
+    
+def morepath(current,mapp):
+    ourpaths = []
+    forks = mapp[current[-1]]
+    #print('Started here: ' ,current[-1],' and possible paths are: ', forks)
+    for e in forks:
+        #print('focus on fork: ', e)
+        if e == 'end':
+            #place holder
+            tmp = current.copy()
+            #print('Path has reached end: ',tmp)
+            ourpaths.append(tmp)
+        else:
+            if not(e in current and (e == e.lower())):
+                tmp = current.copy()
+                tmp.append(e)
+                #print('ended with this path: ', tmp)
+                ourpaths+= morepath(tmp,mapp)
+            #else:
+                #print('Cannot take this path with: ', e,' because small cave again')
+                #print('Path: ', current, ' with: ', e,' after is illegal')
+    #print('our paths are: ',ourpaths,'\n\n')
+    return ourpaths
+
+def morepath2(current,mapp):
+    # For part 2, can visit small cavves at most twice
+    ourpaths = []
+    forks = mapp[current[-1]]
+    #print('Started here: ' ,current[-1],' and possible paths are: ', forks)
+    for e in forks:
+        #print('focus on fork: ', e)
+        if e == 'end':
+            #place holder
+            tmp = current.copy()
+            #print('Path has reached end: ',tmp)
+            ourpaths.append(tmp)
+        else:
+            maxim,lvalues = countlower(current)
+            if e == e.upper() or maxim < 2:
+                tmp = current.copy()
+                tmp.append(e)
+                #print('ended with this path: ', tmp)
+                ourpaths+= morepath2(tmp,mapp)
+            elif e not in lvalues:
+                tmp = current.copy()
+                tmp.append(e)
+                #print('ended with this path: ', tmp)
+                ourpaths+= morepath2(tmp,mapp)
+            #else:
+                #print('Cannot take this path with: ', e,' because small cave again')
+                #print('Path: ', current, ' with: ', e,' after is illegal')
+    #print('our paths are: ',ourpaths,'\n\n')
+    return ourpaths
+
+def allpaths(mapp):
+    starting = mapp['start']
+    apath = []
+    for e in starting:
+        thispath = [e]
+        #print('Start: ', thispath)
+        apath += morepath(thispath,mapp)
+    return apath
+        
+def allpaths2(mapp):
+    # For part 2, can visit small caves at most twice
+    starting = mapp['start']
+    apath = []
+    for e in starting:
+        thispath = [e]
+        #print('Start: ', thispath)
+        apath += morepath2(thispath,mapp)
+    return apath             
 
 cavemap = {}
 
@@ -58,5 +139,8 @@ for a in datas:
         else:
             cavemap[second].append(first)
             
-# paths = possiblepath(cavemap)
+paths = allpaths(cavemap)
+paths2 = allpaths2(cavemap)
     
+print('Part 1 number paths is: ',len(paths))
+print('Part 2 number paths is: ',len(paths2))
